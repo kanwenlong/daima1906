0adf36b0b2cc95a2c24d11b28278662c3808c7e8
kanwenlong <1481408743@qq.com>
2019-06-11 17:51:34 +0800

    Signed-off-by: kanwenlong <1481408743@qq.com>

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+   public static void main(String args[]){
+       System.out.print("Hello World1");
+       System.out.println("Hello World2");
+}
+}
---
+++ zxc.java
@@ -0,0 +1,12 @@
+class HelloWorld{
+   public static void main(String args[]){
+       System.out.print("Hello World1");
+       System.out.println("Hello World2");
+}
+}
+class HelloWorld1{
+   public static void main(String args[]){
+       System.out.println("Hello World1");
+       System.out.println("Hello World2");
+}
+}
